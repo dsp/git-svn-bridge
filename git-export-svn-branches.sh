dsp/git-svn-bridge

commit bb9d5724cff76c3e110c2c10ba57b40d6a94759f

Resolve the full path to the script so it works with both absolute and relative paths

git-export-svn-branches.sh
@@ -22,7 +22,7 @@ svnbranches=(  "trunk"                                    "branches/phpBB-3_0_0"
 gitbranches=(  "develop"                                  "develop-olympus"                          "prep-release-3.0.7" )
 githashinsvn=( "b68de2323d6444b4b3685a98bbcb9500a38e45cb" "d62068cfadcc1478a2f8dd6e7da81dea6cee71ff" "872ad322ec69a032ec22d9e8ae19b9a8399d7712" )
 
-basedir=`dirname $0`
+basedir=`readlink -f \`dirname $0\``
 
 for (( i = 0 ; i < ${#svnbranches[@]} ; i++ ))
     do
@@ -44,7 +44,7 @@ for (( i = 0 ; i < ${#svnbranches[@]} ; i++ ))
             fi
 
         cd $svnbranchname
-        ../$basedir/git-export-svn.sh $gitremote $gitbranch
+        $basedir/git-export-svn.sh $gitremote $gitbranch
         cd ..
     done
 

commit 09f3e157aa9f67008242c802fea83677bf38499e

Fixing the cross-platform issue introduced in bb9d5724cff76c3e110c2c10ba57b40d6a94759f, readlink -f doesn't exist on BSD/OSX

git-export-svn-branches.sh
@@ -22,7 +22,11 @@ svnbranches=(  "trunk"                                    "branches/phpBB-3_0_0"
 gitbranches=(  "develop"                                  "develop-olympus"                          "prep-release-3.0.7" )
 githashinsvn=( "b68de2323d6444b4b3685a98bbcb9500a38e45cb" "d62068cfadcc1478a2f8dd6e7da81dea6cee71ff" "872ad322ec69a032ec22d9e8ae19b9a8399d7712" )
 
-basedir=`readlink -f \`dirname $0\``
+basedir=`dirname $0`
+if [[ $basedir != /* ]]
+then
+    basedir="../$basedir"
+fi
 
 for (( i = 0 ; i < ${#svnbranches[@]} ; i++ ))
     do